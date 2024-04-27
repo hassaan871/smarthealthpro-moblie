c8ee97e285b9706c4849cac9a9416b63adea66a9
arif432 <nawazarif147@gmail.com>
2024-04-27 19:09:12 +0500

    basic chat ui completed

--- screens/chattingUI/BotChattingScreen.js
+++ screens/chattingUI/BotChattingScreen.js
@@ -1,11 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-const BotChattingScreen = () => {
+import { Chat, MessageType, defaultTheme } from '@flyerhq/react-native-chat-ui'
+import React, { useState } from 'react'
+import { View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import darkTheme from '../../Themes/DarkTheme'
+import lightTheme from '../../Themes/LightTheme'
+
+const uuidv4 = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.floor(Math.random() * 16)
+    const v = c === 'x' ? r : (r % 4) + 8
+    return v.toString(16)
+  })
+}
+
+const App = () => {
+  const [messages, setMessages] = useState([])
+  const user = { id: '06c33e8b-e835-4736-80f4-63f44b66666c' }
+
+  const addMessage = (message) => {
+    setMessages([message, ...messages])
+  }
+
+  const handleSendPress = (message) => {
+    const textMessage = {
+      author: user,
+      createdAt: Date.now(),
+      id: uuidv4(),
+      text: message.text,
+      type: 'text',
+    }
+    addMessage(textMessage)
+  }
+
   return (
-    <View>
-      <Text>BotChattingScreen</Text>
-      <Text>asdsadsadsadsada</Text>
-    </View>
+    // Remove this provider if already registered elsewhere
+    // or you have React Navigation set up
+    <SafeAreaProvider>
+      <Chat
+       theme={{
+        ...defaultTheme,
+        // colors: { ...defaultTheme.colors, inputBackground: darkTheme.colors.background},
+        // bac
+      }}
+        messages={messages}
+        onSendPress={handleSendPress}
+        user={user}
+      />
+    </SafeAreaProvider>
   )
 }
-export default BotChattingScreen
+
+export default App

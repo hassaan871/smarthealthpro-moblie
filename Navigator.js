7d38cc6f0a28e12c7c7c766af866f8199afc3170
Zuhair Raza <139696253+ZuhairRaza665@users.noreply.github.com>
2024-04-29 05:27:55 -0400

    Home Screen

--- Navigator.js
+++ Navigator.js
@@ -1,20 +1,29 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import React from 'react';
+import React from "react";
 import BotChattingScreen from "./screens/chattingUI/BotChattingScreen";
+import HomeScreen from "./screens/chattingUI/HomeScreen";
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="BotChattingScreen">
-        <Stack.Screen 
-        name="BotChattingScreen"
-        options={{
-          headerShown: false,
-        }}
-         component={BotChattingScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          options={{
+            headerShown: false,
+          }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="BotChattingScreen"
+          options={{
+            headerShown: false,
+          }}
+          component={BotChattingScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );

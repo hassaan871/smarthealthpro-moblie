b3f72ec639272c8dec8a66b56a188b510b2bfa01
arif432 <nawazarif147@gmail.com>
2024-04-29 13:57:45 +0500

    chatbot data comming connection from backend

--- hooks/chattingHooks/sendFlaskQuery.js
+++ hooks/chattingHooks/sendFlaskQuery.js
@@ -12,27 +12,14 @@ const sendQueryToFlask = async (query) => {
 };
 
 const getFlaskResponse = async () => {
-    try{
-        const response = await axios.get("http://127.0.0.1:5000/");
-        console.log("Response:", response.data.response);
-        return response.data.response;
-    }
-    catch(error){
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            console.error("Error response data:", error.response.data);
-            console.error("Error response status:", error.response.status);
-            console.error("Error response headers:", error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.error("Error request:", error.request);
-        } else {
-            // Something happened in setting up the request that triggered an error
-            console.error("Error message:", error.message);
-        }
-        console.error("Error config:", error.config);
-        return null;
-    }
+  try {
+      const response = await axios.get("http://192.168.100.144");
+      console.log("Response:", response.data);
+      return response.data;
+  } catch(error) {
+      console.error("Error:", error);
+      return null;
+  }
 }
 
 export { sendQueryToFlask, getFlaskResponse };

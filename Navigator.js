b1b79d76345a2216f8a346a2b8a6b6772aad4dfd
Zuhair Raza <139696253+ZuhairRaza665@users.noreply.github.com>
2024-04-29 08:50:23 -0400

    adding tab navigator and settings page

--- Navigator.js
+++ Navigator.js
@@ -1,30 +1,76 @@
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import Icon2 from "react-native-vector-icons/FontAwesome5";
+import lightTheme from "./Themes/LightTheme";
 import BotChattingScreen from "./screens/chattingUI/BotChattingScreen";
 import HomeScreen from "./screens/chattingUI/HomeScreen";
+import Settings from "./screens/chattingUI/Settings";
+import { TouchableOpacity, View } from "react-native";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
+  const [active, setActive] = useState("Home"); // Initialize active tab to "Home"
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          options={{
-            headerShown: false,
-          }}
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="BotChattingScreen"
-          options={{
-            headerShown: false,
-          }}
-          component={BotChattingScreen}
-        />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route, navigation }) => ({
+          headerShown: false,
+          tabBarActiveTintColor: lightTheme.colors.homeActiveTabColor,
+          tabBarInactiveTintColor: lightTheme.colors.homeIconColor,
+          tabBarLabelPosition: "below-icon",
+          tabBarStyle: {},
+          tabBarButton: (props) => (
+            <TouchableOpacity
+              {...props}
+              onPress={() => {
+                navigation.navigate(route.name);
+                setActive(route.name);
+              }}
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              activeOpacity={1} // Finish default opacity effect
+            >
+              {props.children}
+              {active === route.name && (
+                <View
+                  style={{
+                    width: 5,
+                    height: 5,
+                    borderRadius: 5,
+                    backgroundColor: "black",
+                    marginTop: 4,
+                  }}
+                />
+              )}
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Chat") {
+              iconName = "robot";
+            } else if (route.name === "Settings") {
+              iconName = "settings";
+            }
+
+            if (route.name === "Chat") {
+              return <Icon2 name={iconName} size={size} color={color} />;
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Chat" component={BotChattingScreen} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };

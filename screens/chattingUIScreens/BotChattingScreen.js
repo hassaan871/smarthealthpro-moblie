5c7ac9b5a17a39dc12c325397dc154964fe6afdf
arif432 <nawazarif147@gmail.com>
2024-05-03 16:44:51 +0500

    recovered the lost work of mine

--- screens/chattingUIScreens/BotChattingScreen.js
+++ screens/chattingUIScreens/BotChattingScreen.js
@@ -1,68 +1,61 @@
-import { Chat, defaultTheme } from "@flyerhq/react-native-chat-ui";
-import React, { useState } from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-// import ChatHeader from "./components/ChatHeader";
+import { Chat, defaultTheme } from '@flyerhq/react-native-chat-ui';
+import React, { useState } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import ChatHeader from './components/ChatHeader';
 
 const uuidv4 = () => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-    const r = Math.floor(Math.random() * 16);
-    const v = c === "x" ? r : (r % 4) + 8;
-    return v.toString(16);
-  });
-};
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const r = Math.floor(Math.random() * 16);
+        const v = c === 'x' ? r : (r % 4) + 8;
+        return v.toString(16);
+    });
+}
 
 const BotChattingScreen = () => {
-  const [messages, setMessages] = useState([]);
-  const user = { id: "06c33e8b-e835-4736-80f4-63f44b66666c" };
-  const chatbot = { id: "06c33e8b-e899-4736-80f4-63f44b66666c" };
+    const [messages, setMessages] = useState([]);
+    const user = { id: '06c33e8b-e835-4736-80f4-63f44b66666c' };
+    const chatbot = { id: "06c33e8b-e899-4736-80f4-63f44b66666c" };
 
-  const addMessage = (message) => {
-    setMessages((prevMessages) => [message, ...prevMessages]);
-  };
+    const addMessage = (message) => {
+        setMessages((prevMessages) => [message,...prevMessages]);
+    }
 
-  const handleSendPress = (message) => {
-    const sentMessage = {
-      author: user,
-      createdAt: Date.now(),
-      id: uuidv4(),
-      text: message.text,
-      type: "text",
-    };
-    addMessage(sentMessage);
+    const handleSendPress = (message) => {
+        const sentMessage = {
+            author: user,
+            createdAt: Date.now(),
+            id: uuidv4(),
+            text: message.text,
+            type: 'text',
+        };
+        addMessage(sentMessage);
 
-    setTimeout(() => {
-      const receivedMessage = {
-        author: chatbot,
-        createdAt: Date.now(),
-        id: uuidv4(),
-        text: "yes you are right ",
-        type: "text",
-      };
-      addMessage(receivedMessage);
-    }, 500);
-  };
+        setTimeout(() => {
+            const receivedMessage = {
+                author: chatbot,
+                createdAt: Date.now(),
+                id: uuidv4(),
+                text: "yes you are right ",
+                type: 'text',
+            };
+            addMessage(receivedMessage);
+        }, 500);
+    }
 
-  return (
-    <SafeAreaProvider>
-      {/* <ChatHeader icons={true} /> */}
-      <Chat
-        // change text message background
-        theme={{
-          ...defaultTheme,
-          colors: {
-            ...defaultTheme.colors,
-            primary: "#3182ce",
-            background: "#f7fafc",
-            inputBackground: "#3182ce",
-          },
-        }}
-        messages={messages}
-        onSendPress={handleSendPress}
-        user={user}
-        inverted={true} // Ensure new messages appear at the bottom
-      />
-    </SafeAreaProvider>
-  );
-};
+    return (
+        <SafeAreaProvider>
+            <ChatHeader icons={true} />
+            <Chat
+            // change text message background
+                theme={{ ...defaultTheme, colors: 
+                    { ...defaultTheme.colors, primary: '#3182ce', background: '#f7fafc',inputBackground: '#3182ce'}}}
+                messages={messages}
+                onSendPress={handleSendPress}
+                user={user}
+                inverted={true} // Ensure new messages appear at the bottom
+            />
+        </SafeAreaProvider>
+    );
+}
 
 export default BotChattingScreen;

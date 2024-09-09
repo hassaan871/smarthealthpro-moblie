7d64803519ab6411a59d73ebab2f974fac7c065b
arif432 <nawazarif147@gmail.com>
2024-09-09 21:34:39 +0500

    working on extrection of priority

--- screens/extras/ChatScreen2.js
+++ screens/extras/ChatScreen2.js
@@ -42,7 +42,7 @@ const ChatsScreen = () => {
       console.log("userinfo id: ", userInfo);
       try {
         const response = await axios.get(
-          `http://192.168.100.132:5000/conversations/${userInfo?._id}`
+          `http://192.168.100.169:5000/conversations/${userInfo?._id}`
         );
         console.log("fetched chats 2332: ", response.data);
 
